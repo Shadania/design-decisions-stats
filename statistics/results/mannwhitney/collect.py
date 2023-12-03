54a426e107adc0240172da52d8175c2c8eee46af
Sarah Druyts <sarah.druyts@gmail.com>
2023-12-03 15:54:42 +0100

    update collect.py to include rq2

--- statistics/results/mannwhitney/collect.py
+++ statistics/results/mannwhitney/collect.py
@@ -10,6 +10,10 @@ characs = [
     "watches",
 ]
 
+import os
+os.makedirs('forlatex/rq3/', exist_ok=True)
+os.makedirs('forlatex/rq2/', exist_ok=True)
+
 for charac in characs:
     files = [
         f"{charac}_simple_plot_arrows",
@@ -18,4 +22,6 @@ for charac in characs:
         f"{charac}_inverted_plot_arrows"
     ]
     for file in files:
-        shutil.copy(f"rq3_{charac}/{file}.png", f"forlatex/{file}.png")
+        shutil.copy(f"rq3_{charac}/{file}.png", f"forlatex/rq3/{file}.png")
+    file = f"{charac}_plot_arrows.png"
+    shutil.copy(f"rq2_{charac}/{file}", f"forlatex/rq2/{file}")
